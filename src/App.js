96b92d15c86a882e89f01045d75c67f31f6922db
Toniolo-Marco <marcotoniolomail@gmail.com>
2021-01-09 22:00:27 +0100

    added chart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Counter from "./components/counter";
 import { inttoString } from "./utils/string.manipulation";
 import styles from "./scss/global.module.scss"
 import { deathPerSecond } from "./utils/statistics";
+import MyChart from "./components/chart";
 
 function App() {
   const [number, setNumber] = useState (157_000);
@@ -12,7 +13,7 @@ function App() {
     <div className={styles.main}>
 
       <Counter init={number} />
-
+      <MyChart></MyChart>
     </div>
   );
 }
